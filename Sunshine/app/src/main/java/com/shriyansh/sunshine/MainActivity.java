c3a7d25ad22ce66634e18215d9e5b3b815498932
Shriyansh Gautam <shriyansh.gautam.cse13@iitbhu.ac.in>
2015-03-09 14:43:19 +0530

    Moved Forecastfragment to a new file with NetworonMainThread

--- Sunshine/app/src/main/java/com/shriyansh/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/com/shriyansh/sunshine/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -61,100 +61,5 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            String forecastList[]={
-                    "Today - Sunny  -  88 / 63",
-                    "Monday 16 - Clear  -  45 / 48",
-                    "Tuesday 17 - Rain  -  16 / 49",
-                    "Thursday 18- Sunny  -  88 / 63",
-                    "Friday 19 - Clear  -  45 / 48",
-                    "Saturday 20 - Rain  -  16 / 49",
-                    "Sunday 21 - Sunny  -  88 / 63",
-                    "Monday 22 - Clear  -  45 / 48",
-                    "Tuesday 23 - Rain  -  16 / 49",
-                    "Thursday 24 - Sunny  -  88 / 63",
-                    "Friday 25 - Clear  -  45 / 48",
-                    "Saturday 26 - Rain  -  16 / 49"
-            };
-
-            List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastList));
-
-            ArrayAdapter<String> forecastAdapter= new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textview,forecastList);
-            ListView listView=(ListView)rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(forecastAdapter);
-
-
-            // These two need to be declared outside the try/catch
-            // so that they can be closed in the finally block.
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-
-            // Will contain the raw JSON response as a string.
-            String forecastJsonStr = null;
-
-            try {
-            // Construct the URL for the OpenWeatherMap query
-            // Possible parameters are avaiable at OWM's forecast API page, at
-            // http://openweathermap.org/API#forecast
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
-
-            // Create the request to OpenWeatherMap, and open the connection
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                // Read the input stream into a String
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null) {
-                // Nothing to do.
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                // But it does make debugging a *lot* easier if you print out the completed
-                // buffer for debugging.
-                    buffer.append(line + "\n");
-                }
-
-                if (buffer.length() == 0) {
-                // Stream was empty. No point in parsing.
-                    return null;
-                }
-                forecastJsonStr = buffer.toString();
-            } catch (IOException e) {
-                Log.e("PlaceholderFragment", "Error ", e);
-                // If the code didn't successfully get the weather data, there's no point in attemping
-                // to parse it.
-                return null;
-            } finally{
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e("PlaceholderFragment", "Error closing stream", e);
-                    }
-                }
-            }
-
-            return rootView;
-        }
-    }
 }
