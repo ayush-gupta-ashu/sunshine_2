5b1242ab1374c93db98cdccb178d5d14102a5b31
Shriyansh Gautam <shriyansh.gautam.cse13@iitbhu.ac.in>
2015-03-09 15:22:42 +0530

    Added Menu to ForecastFragment

--- Sunshine/app/src/main/java/com/shriyansh/sunshine/ForecastFragment.java
+++ Sunshine/app/src/main/java/com/shriyansh/sunshine/ForecastFragment.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -25,7 +28,31 @@ import java.util.List;
 public class ForecastFragment extends Fragment {
 
     public ForecastFragment() {
+
+
     }
+
+    /**
+     * Called to do initial creation of a fragment.  This is called after
+     * {@link #onAttach(android.app.Activity)} and before
+     * {@link #onCreateView(android.view.LayoutInflater, android.view.ViewGroup, android.os.Bundle)}.
+     * <p/>
+     * <p>Note that this can be called while the fragment's activity is
+     * still in the process of being created.  As such, you can not rely
+     * on things like the activity's content view hierarchy being initialized
+     * at this point.  If you want to do work once the activity itself is
+     * created, see {@link #onActivityCreated(android.os.Bundle)}.
+     *
+     * @param savedInstanceState If the fragment is being re-created from
+     *                           a previous saved state, this is the state.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -51,12 +78,61 @@ public class ForecastFragment extends Fragment {
         ArrayAdapter<String> forecastAdapter= new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textview,forecastList);
         ListView listView=(ListView)rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(forecastAdapter);
-        
+
         return rootView;
     }
 
+    /**
+     * Initialize the contents of the Activity's standard options menu.  You
+     * should place your menu items in to <var>menu</var>.  For this method
+     * to be called, you must have first called {@link #setHasOptionsMenu}.  See
+     * {@link android.app.Activity#onCreateOptionsMenu(android.view.Menu) Activity.onCreateOptionsMenu}
+     * for more information.
+     *
+     * @param menu     The options menu in which you place your items.
+     * @param inflater
+     * @see #setHasOptionsMenu
+     * @see #onPrepareOptionsMenu
+     * @see #onOptionsItemSelected
+     */
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecast_fragment,menu);
+    }
 
-   public class FetchWeatherTask extends AsyncTask<Void,Void,Void>{
+    /**
+     * This hook is called whenever an item in your options menu is selected.
+     * The default implementation simply returns false to have the normal
+     * processing happen (calling the item's Runnable or sending a message to
+     * its Handler as appropriate).  You can use this method for any items
+     * for which you would like to do processing without those other
+     * facilities.
+     * <p/>
+     * <p>Derived classes should call through to the base class for it to
+     * perform the default menu handling.
+     *
+     * @param item The menu item that was selected.
+     * @return boolean Return false to allow normal menu processing to
+     * proceed, true to consume it here.
+     * @see #onCreateOptionsMenu
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id=item.getItemId();
+
+        if(id==R.id.action_refresh){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+    public class FetchWeatherTask extends AsyncTask<Void,Void,Void>{
+
+       String LOG_TAG=ForecastFragment.class.getSimpleName();
 
        /**
         * Override this method to perform a computation on a background thread. The
@@ -117,7 +193,7 @@ public class ForecastFragment extends Fragment {
                }
                forecastJsonStr = buffer.toString();
            } catch (IOException e) {
-               Log.e("PlaceholderFragment", "Error ", e);
+               Log.e(LOG_TAG, "Error ", e);
                // If the code didn't successfully get the weather data, there's no point in attemping
                // to parse it.
                return null;
@@ -129,7 +205,7 @@ public class ForecastFragment extends Fragment {
                    try {
                        reader.close();
                    } catch (final IOException e) {
-                       Log.e("PlaceholderFragment", "Error closing stream", e);
+                       Log.e(LOG_TAG, "Error closing stream", e);
                    }
                }
            }
